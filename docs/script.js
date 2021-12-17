334ee00ceed24cc9300848e8658f5b4c88e582d0
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-17 11:47:34 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -48,9 +48,35 @@ class Classi {
 
         var table = document.createElement('table');
         var caption = document.createElement('caption');
-        var textCaption = document.createTextNode("Dati caricati per creare file csv");
+        var textCaption = document.createTextNode("dati caricati per creare file csv");
         caption.appendChild(textCaption);
         table.appendChild(caption)
+
+        var th1 = document.createElement('th');
+        var textth1 = document.createTextNode("id");
+        th1.appendChild(textth1);
+
+        var th2 = document.createElement('th');
+        var textth2 = document.createTextNode("usr");
+        th2.appendChild(textth2);
+
+        var th3 = document.createElement('th');
+        var textth3 = document.createTextNode("psw");
+        th3.appendChild(textth3);
+
+        var th4 = document.createElement('th');
+        var textth4 = document.createTextNode("email");
+        th4.appendChild(textth4);
+
+        var th5 = document.createElement('th');
+        var textth5 = document.createTextNode("age");
+        th5.appendChild(textth5;
+
+        table.appendChild(th1);
+        table.appendChild(th2);
+        table.appendChild(th3);
+        table.appendChild(th4);
+        table.appendChild(th5);
 
         divContent.appendChild(table);
 
