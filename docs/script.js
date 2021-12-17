f4db1ec648b164d618155d5bfd1461ee5e2b08d9
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-17 14:18:43 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -143,6 +143,42 @@ class Classi {
      divContent.appendChild(table);
      divContent.appendChild(btn);
     } // create table
+    // scarica file csv
+    downloadCSVFile(csv, filename) {
+        var csv_file, download_link;
+
+        csv_file = new Blob([csv], {type: "text/csv"});
+
+        download_link = document.createElement("a");
+
+        download_link.download = filename;
+
+        download_link.href = window.URL.createObjectURL(csv_file);
+
+        download_link.style.display = "none";
+
+        document.body.appendChild(download_link);
+
+        download_link.click();
+    } // ./ scarica file csv
+    // crea file csv
+    htmlToCSV(html, filename) {
+        var data = [];
+        var rows = document.querySelectorAll("table tr");
+        for (var i = 0; i < rows.length; i++) {
+            var row = [], cols = rows[i].querySelectorAll("td, th");
+    
+            for (var j = 0; j < cols.length; j++) {
+                    row.push(cols[j].innerText);
+            }
+                    
+            data.push(row.join(",")); 		
+        }
+    
+        this.downloadCSVFile(data.join("\n"), filename);
+        // console.table(data[0]);
+    // ./crea file csv
+    }
 } // ./classi
 
 // call Classi
@@ -153,3 +189,11 @@ cls.createTable();
 // call Funzioni
 // Funzioni.messaggioDiBenvenuto();
 // Funzioni.showMyAlert("primo messaggio di alert", "var(--success)", "var(--white)", "var(--yellow)");
+
+// func create file csv
+
+
+var btnScaricaFileCsv = document.querySelector('.btn-scarica-file-csv');
+btnScaricaFileCsv.addEventListener('click', function() {
+    htmlToCSV(null, "File-csv")
+});
