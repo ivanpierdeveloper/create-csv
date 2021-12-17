be9a8920d0e15446c1511970924b7765b34f3f86
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-17 09:42:11 +0100

    ivanpier

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+'use strict'
+const funzioni = {
+    messaggioDiBenvenuto : function() {
+        alert("| Welcome To Back |");
+    }
+}// ./funzioni
+class Classi {
+    // metodi
+    messaggioDiConferma(msg) {
+        alert(`| ${msg} |`); // ` = alt(sx)+96(tastierino numerico)
+    }
+} // ./classi
