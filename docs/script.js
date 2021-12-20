b123dabdcd46f2e9422530bd66e3a9a5161b7822
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-20 17:39:30 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -222,7 +222,7 @@ btnScaricaFileCsv.addEventListener('click', function() {
     var question = prompt("Nome file", "Nome file CSV");
     if(question) {
         cls.htmlToCSV(null, question);
-    } else if(question == null){
+    } else if(question == ""){
         Funzioni.showMyAlert("non hai scritto nome file", "var(--warning)", "var(--dark)", "var(--dark)");
     }
 });
